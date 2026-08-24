134a7fb12e00ce81f5f7ba3d01436d5655669c49
Nikhil Akula <nikhil.akula21@gmail.com>
2026-08-24 19:30:28 +0000

    Refactor project structure to integrate Spring Boot and remove legacy Main class and tests

--- src/main/java/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hey this is team Leap-Laugh-Love");
-    }
-}
--- src/test/java/com/leap/leaplaughlove/MainTest.java
+++ src/test/java/com/leap/leaplaughlove/MainTest.java
@@ -1,3 +1,5 @@
+package com.leap.leaplaughlove;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
