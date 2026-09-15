7be83525f9937c90d48bdd13e96d9cf3e172221b
Chris Musselman <christopher.musselman04@gmail.com>
2026-09-15 20:32:08 +0000

    feat: created repo interfaces for exec/instrument/position moves, and position moves entity for storing order sub data

---
+++ trading-app/src/main/java/com/leap/leaplaughlove/trading/order/ExecutionRepository.java
@@ -0,0 +1,13 @@
+package com.leap.leaplaughlove.trading.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ExecutionRepository extends JpaRepository<Execution, UUID> {
+    List<Execution> findByOrder_OrderId(UUID orderId);
+}
+
